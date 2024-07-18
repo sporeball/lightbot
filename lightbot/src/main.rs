44b4066193d39abf0837334b9a3a1050a9b24297
sporeball <sporeballdev@gmail.com>
2024-07-17 23:02:34 -0700

    spellcheck

--- lightbot/src/main.rs
+++ lightbot/src/main.rs
@@ -4,21 +4,44 @@ use serenity::model::channel::Message;
 use serenity::model::gateway::Ready;
 use serenity::prelude::*;
 use shuttle_runtime::SecretStore;
+use symspell::{AsciiStringStrategy, SymSpell, Verbosity};
 use tracing::{error, info};
 
-struct Bot;
+struct Bot {
+  symspell: SymSpell<AsciiStringStrategy>,
+}
 
 #[async_trait]
 impl EventHandler for Bot {
   async fn message(&self, ctx: Context, msg: Message) {
-    if msg.content == "!hello" {
-      if let Err(e) = msg.channel_id.say(&ctx.http, "world!").await {
-        error!("Error sending message: {:?}", e);
-      }
+    // if msg.content == "!hello" {
+    //   if let Err(e) = msg.channel_id.say(&ctx.http, "world!").await {
+    //     error!("Error sending message: {:?}", e);
+    //   }
+    // }
+    // info!("lightbot: msg.content  -> bool= {}", msg.content);
+    // info!("lightbot: msg.mentions = {:?}", msg.mentions);
+    if !msg.mentions_lightbot() {
+      return
     }
+    let content = &msg.content[22..]
+      .trim();
+    info!("[m] '{}'", content);
+    let corrected = &self.symspell.lookup_compound(content, 2)[0];
+    info!("[m] c: '{}' (dst: {})", corrected.term, corrected.distance);
   }
   async fn ready(&self, _: Context, ready: Ready) {
     info!("{} is connected!", ready.user.name);
+  }
+}
+
+trait MentionsLightbot {
+  fn mentions_lightbot(&self) -> bool;
+}
+
+impl MentionsLightbot for Message {
+  fn mentions_lightbot(&self) -> bool {
+    self.content.starts_with("<@1263328534030454875>")
   }
 }
 
@@ -34,8 +57,20 @@ async fn serenity(
   // Set gateway intents, which decides what events the bot will be notified about
   let intents = GatewayIntents::GUILD_MESSAGES | GatewayIntents::MESSAGE_CONTENT;
 
+  let mut symspell: SymSpell<AsciiStringStrategy> = SymSpell::default();
+  info!("loading dictionary...");
+  symspell.load_dictionary("data/frequency_dictionary_en_82_765.txt", 0, 1, " ");
+  info!("loaded dictionary!");
+  info!("loading bigram dictionary...");
+  symspell.load_bigram_dictionary("data/frequency_bigramdictionary_en_243_342.txt", 0, 2, " ");
+  info!("loaded bigram dictionary!");
+
+  let bot = Bot {
+    symspell,
+  };
+
   let client = Client::builder(&token, intents)
-    .event_handler(Bot)
+    .event_handler(bot)
     .await
     .expect("Err creating client");
 
